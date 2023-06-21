735fd6b78e5f43ace1092336f19399a928c51cb5
SepehrShahraeini <shahraeinsepehr@gmail.com>
2023-06-21 13:39:19 +0330

    First release of Negs archive

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+
+from telethon import TelegramClient, events
+api_id = 14498488
+api_hash = '7273c3ef3d48b1a2aa47a1ecaf39eb4b'
+client = TelegramClient('sepehr', api_id, api_hash)
+client.start()
+
+@client.on(events.NewMessage(chats='MyGreenSilence'))
+async def my_event_handler(event):
+    await client.forward_messages('negsarch', event.message)
+
+with client:
+    client.run_until_disconnected()
