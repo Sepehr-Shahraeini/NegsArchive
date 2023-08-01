32dbc6b221283ab6df676100f646af1d03f8093a
Sepehr-Shahraeini <shahraeinisepehr@gmail.com>
2023-08-01 16:42:52 +0330

    code updated to forward message into private channel

--- NegsArchive.py
+++ NegsArchive.py
@@ -6,8 +6,9 @@ client = TelegramClient('sepehr', api_id, api_hash)
 client.start()
 print("client started")
 @client.on(events.NewMessage(chats='MyGreenSilence'))
+
 async def my_event_handler(event):
-    await client.forward_messages('negsarch', event.message)
+    await client.forward_messages('https://t.me/+bGV4ikMTLjswZTc8', event.message)
 
 with client:
     client.run_until_disconnected()
